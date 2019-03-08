c9a591942884e4a141feac79b22e86997b0b7a41
light6607 <740415858@qq.com>
2019-03-08 09:46:39 +0800

    change the log

--- light/collect_script/backgroud_flow.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python Rs1.py & python Rs2.py & python Rs3.py
--- light/collect_script/old_imitate/bak_flow.sh
+++ light/collect_script/old_imitate/bak_flow.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/bin/bash rs1.sh && /bin/bash rs2.sh && /bin/bash rs3.sh
+/bin/bash rs1.sh &
+/bin/bash rs2.sh &
+/bin/bash rs3.sh &
+
+# 避免出现意外终止的情况，python2的编码问题，字符集大于255.所以采用这种方式来做守护进程
+# 如何终止？
+# ps -ef | grep 'python Rs' | awk  '{print "kill -9 " $3}' | sh
+# ps -ef | grep 'python Rs' | awk  '{print "kill -9 " $2}' | sh
+# 顺序千万别错！
